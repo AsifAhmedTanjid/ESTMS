b2ea20f11b818793e7a593adb142027bbf1f02f1
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2023-12-30 22:10:57 +0600

    user added

---
+++ APIAppLayer/DAL/Migrations/202312291912499_userTableUpdated.cs
@@ -0,0 +1,22 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class userTableUpdated : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Users", "Username", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Users", "Password", c => c.String(nullable: false, maxLength: 20));
+            AlterColumn("dbo.Users", "Email", c => c.String(nullable: false, maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Users", "Email", c => c.String());
+            AlterColumn("dbo.Users", "Password", c => c.String());
+            AlterColumn("dbo.Users", "Username", c => c.String());
+        }
+    }
+}
